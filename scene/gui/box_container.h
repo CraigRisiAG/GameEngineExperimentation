bbaf49f3ddff2180fd2a4fcaaf3b0b34cdcb5721
Craig Risi <sandelk@gmail.com>
2026-09-16 10:57:16 +0200

    update scene code and documentation

--- scene/gui/box_container.h
+++ scene/gui/box_container.h
@@ -1,38 +1,3 @@
-/*************************************************************************/
-/*  box_container.h                                                      */
-/*************************************************************************/
-/*                       This file is part of:                           */
-/*                           GODOT ENGINE                                */
-
-/**
- * @file box_container.h
- * @brief Implementation of BoxContainer class.
- */
-
-/*                      https://godotengine.org                          */
-/*************************************************************************/
-/* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
-/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
-/*                                                                       */
-/* Permission is hereby granted, free of charge, to any person obtaining */
-/* a copy of this software and associated documentation files (the       */
-/* "Software"), to deal in the Software without restriction, including   */
-/* without limitation the rights to use, copy, modify, merge, publish,   */
-/* distribute, sublicense, and/or sell copies of the Software, and to    */
-/* permit persons to whom the Software is furnished to do so, subject to */
-/* the following conditions:                                             */
-/*                                                                       */
-/* The above copyright notice and this permission notice shall be        */
-/* included in all copies or substantial portions of the Software.       */
-/*                                                                       */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
-/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
-/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
-/*************************************************************************/
 
 #ifndef BOX_CONTAINER_H
 #define BOX_CONTAINER_H
@@ -41,56 +6,51 @@
 
 class BoxContainer : public Container {
 
-	GDCLASS(BoxContainer, Container);
+  GDCLASS(BoxContainer, Container);
 
 public:
-	enum AlignMode {
-		ALIGN_BEGIN,
-		ALIGN_CENTER,
-		ALIGN_END
-	};
+  enum AlignMode { ALIGN_BEGIN, ALIGN_CENTER, ALIGN_END };
 
 private:
-	bool vertical;
-	AlignMode align;
+  bool vertical;
+  AlignMode align;
 
-	void _resort();
+  void _resort();
 
 protected:
-	void _notification(int p_what);
+  void _notification(int p_what);
 
-	static void _bind_methods();
+  static void _bind_methods();
 
 public:
-	void add_spacer(bool p_begin = false);
+  void add_spacer(bool p_begin = false);
 
-	void set_alignment(AlignMode p_align);
-	AlignMode get_alignment() const;
+  void set_alignment(AlignMode p_align);
+  AlignMode get_alignment() const;
 
-	virtual Size2 get_minimum_size() const;
+  virtual Size2 get_minimum_size() const;
 
-	BoxContainer(bool p_vertical = false);
+  BoxContainer(bool p_vertical = false);
 };
 
 class HBoxContainer : public BoxContainer {
 
-	GDCLASS(HBoxContainer, BoxContainer);
+  GDCLASS(HBoxContainer, BoxContainer);
 
 public:
-	HBoxContainer() :
-			BoxContainer(false) {}
+  HBoxContainer() : BoxContainer(false) {}
 };
 
 class MarginContainer;
 class VBoxContainer : public BoxContainer {
 
-	GDCLASS(VBoxContainer, BoxContainer);
+  GDCLASS(VBoxContainer, BoxContainer);
 
 public:
-	MarginContainer *add_margin_child(const String &p_label, Control *p_control, bool p_expand = false);
+  MarginContainer *add_margin_child(const String &p_label, Control *p_control,
+                                    bool p_expand = false);
 
-	VBoxContainer() :
-			BoxContainer(true) {}
+  VBoxContainer() : BoxContainer(true) {}
 };
 
 VARIANT_ENUM_CAST(BoxContainer::AlignMode);
--- scene/gui/container.h
+++ scene/gui/container.h
@@ -1,38 +1,3 @@
-/*************************************************************************/
-/*  container.h                                                          */
-/*************************************************************************/
-/*                       This file is part of:                           */
-/*                           GODOT ENGINE                                */
-
-/**
- * @file container.h
- * @brief Implementation of Container class.
- */
-
-/*                      https://godotengine.org                          */
-/*************************************************************************/
-/* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
-/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
-/*                                                                       */
-/* Permission is hereby granted, free of charge, to any person obtaining */
-/* a copy of this software and associated documentation files (the       */
-/* "Software"), to deal in the Software without restriction, including   */
-/* without limitation the rights to use, copy, modify, merge, publish,   */
-/* distribute, sublicense, and/or sell copies of the Software, and to    */
-/* permit persons to whom the Software is furnished to do so, subject to */
-/* the following conditions:                                             */
-/*                                                                       */
-/* The above copyright notice and this permission notice shall be        */
-/* included in all copies or substantial portions of the Software.       */
-/*                                                                       */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
-/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
-/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
-/*************************************************************************/
 
 #ifndef CONTAINER_H
 #define CONTAINER_H
@@ -41,31 +6,29 @@
 
 class Container : public Control {
 
-	GDCLASS(Container, Control);
+  GDCLASS(Container, Control);
 
-	bool pending_sort;
-	void _sort_children();
-	void _child_minsize_changed();
+  bool pending_sort;
+  void _sort_children();
+  void _child_minsize_changed();
 
 protected:
-	void queue_sort();
-	virtual void add_child_notify(Node *p_child);
-	virtual void move_child_notify(Node *p_child);
-	virtual void remove_child_notify(Node *p_child);
+  void queue_sort();
+  virtual void add_child_notify(Node *p_child);
+  virtual void move_child_notify(Node *p_child);
+  virtual void remove_child_notify(Node *p_child);
 
-	void _notification(int p_what);
-	static void _bind_methods();
+  void _notification(int p_what);
+  static void _bind_methods();
 
 public:
-	enum {
-		NOTIFICATION_SORT_CHILDREN = 50
-	};
+  enum { NOTIFICATION_SORT_CHILDREN = 50 };
 
-	void fit_child_in_rect(Control *p_child, const Rect2 &p_rect);
+  void fit_child_in_rect(Control *p_child, const Rect2 &p_rect);
 
-	virtual String get_configuration_warning() const;
+  virtual String get_configuration_warning() const;
 
-	Container();
+  Container();
 };
 
 #endif // CONTAINER_H
