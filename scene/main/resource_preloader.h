504060585f29735c76c39e73f272f98a78b94c8f
Craig Risi <sandelk@gmail.com>
2026-09-17 15:35:30 +0200

    update scene main code

--- scene/main/resource_preloader.h
+++ scene/main/resource_preloader.h
@@ -1,38 +1,3 @@
-/*************************************************************************/
-/*  resource_preloader.h                                                 */
-/*************************************************************************/
-/*                       This file is part of:                           */
-/*                           GODOT ENGINE                                */
-
-/**
- * @file resource_preloader.h
- * @brief Pre-loads resources at scene startup.
- */
-
-/*                      https://godotengine.org                          */
-/*************************************************************************/
-/* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
-/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
-/*                                                                       */
-/* Permission is hereby granted, free of charge, to any person obtaining */
-/* a copy of this software and associated documentation files (the       */
-/* "Software"), to deal in the Software without restriction, including   */
-/* without limitation the rights to use, copy, modify, merge, publish,   */
-/* distribute, sublicense, and/or sell copies of the Software, and to    */
-/* permit persons to whom the Software is furnished to do so, subject to */
-/* the following conditions:                                             */
-/*                                                                       */
-/* The above copyright notice and this permission notice shall be        */
-/* included in all copies or substantial portions of the Software.       */
-/*                                                                       */
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
-/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
-/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
-/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
-/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
-/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
-/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
-/*************************************************************************/
 
 #ifndef RESOURCE_PRELOADER_H
 #define RESOURCE_PRELOADER_H
@@ -41,27 +6,28 @@
 
 class ResourcePreloader : public Node {
 
-	GDCLASS(ResourcePreloader, Node);
+  GDCLASS(ResourcePreloader, Node);
 
-	Map<StringName, RES> resources;
+  Map<StringName, RES> resources;
 
-	void _set_resources(const Array &p_data);
-	Array _get_resources() const;
-	Vector<String> _get_resource_list() const;
+  void _set_resources(const Array &p_data);
+  Array _get_resources() const;
+  Vector<String> _get_resource_list() const;
 
 protected:
-	static void _bind_methods();
+  static void _bind_methods();
 
 public:
-	void add_resource(const StringName &p_name, const RES &p_resource);
-	void remove_resource(const StringName &p_name);
-	void rename_resource(const StringName &p_from_name, const StringName &p_to_name);
-	bool has_resource(const StringName &p_name) const;
-	RES get_resource(const StringName &p_name) const;
+  void add_resource(const StringName &p_name, const RES &p_resource);
+  void remove_resource(const StringName &p_name);
+  void rename_resource(const StringName &p_from_name,
+                       const StringName &p_to_name);
+  bool has_resource(const StringName &p_name) const;
+  RES get_resource(const StringName &p_name) const;
 
-	void get_resource_list(List<StringName> *p_list);
+  void get_resource_list(List<StringName> *p_list);
 
-	ResourcePreloader();
+  ResourcePreloader();
 };
 
 #endif // RESOURCE_PRELOADER_H
